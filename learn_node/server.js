822e26865362a5233188494b76831d5a2a4bee4f
fanyj1994 <946020997@qq.com>
2019-06-28 15:47:42 +0800

    学习node 测试commit换行

--- learn_node/server.js
+++ learn_node/server.js
@@ -1,8 +1,18 @@
 let data = require('./data')
+const db = require('diskdb')
 const express = require('express');
 const body_parser = require('body-parser')
 const server = express();
 const port = 4000;
+
+db.connect('./data', ['movies'])
+
+if (!db.movies.find().length) {
+  const movie = {id: 'tt0110357', name: 'The Lion King', genre: 'animation'}
+  db.movies.save(movie)
+}
+
+console.log(db.movies.find())
 
 server.use(body_parser.json())
 
@@ -16,10 +26,10 @@ server.get('/', (req, res) => {
 
 server.get('/items/:id', (req, res) => {
   const itemId = req.params.id;
-  const item = data.find(_item => _item.id === itemId);
+  const items = db.movies.find({id: itemId})
 
-  if (item) {
-    res.json(item);
+  if (items.length) {
+    res.json(items);
   } else {
     res.json({ message: `item ${itemId} doesn't exist`})
   }
@@ -33,8 +43,9 @@ server.post('/items', (req, res) => {
   const item = req.body;
   console.log('Adding new item:', item);
 
-  data.push(item)
-  res.json(data)
+  db.movies.save(item)
+
+  res.json(db.movies.find())
 })
 
 server.put('/items/:id', (req, res) => {
@@ -42,27 +53,19 @@ server.put('/items/:id', (req, res) => {
   const item = req.body;
   console.log('Editing item: ' + itemId, ' to be ', item);
 
-  const updatedListItems = [];
+  db.movies.update({ id: itemId }, item)
 
-  data.forEach(oldItem => {
-    if (oldItem.id === itemId) {
-      updatedListItems.push(item);
-    } else {
-      updatedListItems.push(oldItem);
-    }
-  })
-
-  data = updatedListItems;
-
-  res.json(data);
+  res.json(db.movies.find());
 })
 
 server.delete('/items/:id', (req, res) => {
   const itemId = req.params.id;
   console.log('Delete item ', itemId);
+
+  db.movies.remove({ id: itemId })
   data = data.filter(item => item.id !== itemId)
 
-  res.json(data);
+  res.json(db.movies.find());
 })
 
 server.listen(port, () => {
